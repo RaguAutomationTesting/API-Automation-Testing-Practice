37bceecfc7abc1d10efbeda3f196ff1df3f1621d
RaguAutomationTesting <raguaman3008@gmail.com>
2025-05-17 11:47:27 +0530

    Commmited by SubB1

---
+++ src/test/java/TestSample/JsonLibrary.java
@@ -0,0 +1,57 @@
+package TestSample;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+public class JsonLibrary {
+	
+	
+	
+	@Test (priority =1)
+	void testPostUsingHashMap() {
+		
+		
+		JSONObject data = new JSONObject();
+		
+		data.put("name", "Scott");
+		data.put("sex", "Male");
+		data.put("age","23");
+		
+		 
+		
+		
+		
+		
+		given()
+			.contentType("application/json")
+			.body(data.toString())
+		.when()
+			.put("http://localhost:3000/StudentDetails")
+		.then()
+			.statusCode(200)
+			.body("name",equalTo("Scott"))
+			.body("sex",equalTo("Male"))
+			.log().all();
+		
+	}
+	
+	@Test(priority =2)
+	void testDeleteUser() {
+		
+		given()
+			.when()
+				.delete("http://localhost:3000/StudentDetails/")
+		.then()
+		//Changed by SubB1
+		.statusCode(200);
+		
+		
+	}
+
+
+}
